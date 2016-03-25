31766851e60376bce749728719303b423b4aedd6
onasanderr <onasanderr@gmail.com>
2016-03-25 10:27:20 -0500

    add buy in

--- player.rb
+++ player.rb
@@ -6,7 +6,7 @@ class Player
 
   def bet_request(game_state)
     pp game_state, '*' * 80, game_state["current_buy_in"]
-    (100..1000).to_a.shuffle.sample
+    (game_state["current_buy_in"]..1000).to_a.shuffle.sample
   end
 
   def showdown(game_state)
