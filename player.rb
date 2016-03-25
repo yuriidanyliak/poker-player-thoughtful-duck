001cf32fa0508e94f87b7fa732000ea7b46b0db7
onasanderr <onasanderr@gmail.com>
2016-03-25 10:19:41 -0500

    add pp

--- player.rb
+++ player.rb
@@ -1,14 +1,15 @@
+require 'pp'
 
 class Player
 
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    p game_state, '*' * 80, game_state["current_buy_in"]
+    pp game_state, '*' * 80, game_state["current_buy_in"]
     (100..1000).to_a.shuffle.sample
   end
 
   def showdown(game_state)
-
+    pp game_state
   end
 end
