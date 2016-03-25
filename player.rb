b33aeaa63fed5fa25ef0ecc48c79e108e6da81d5
onasanderr <onasanderr@gmail.com>
2016-03-25 10:02:47 -0500

    fix

--- player.rb
+++ player.rb
@@ -5,7 +5,7 @@ class Player
 
   def bet_request(game_state)
     minimum_raise = 0
-    game_state['current_buy_in'] - game_state['players']['in_action']['bet']  + minimum_raise
+    game_state['current_buy_in'].to_i - game_state['players']['in_action']['bet'].to_i  + minimum_raise
   end
 
   def showdown(game_state)
