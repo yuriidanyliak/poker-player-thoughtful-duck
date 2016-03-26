15ceade019d035384d9cf8602637fbb63bc38fb2
butumusho <danuljak@gmail.com>
2016-03-26 11:05:54 +0200

    6

--- player.rb
+++ player.rb
@@ -6,7 +6,7 @@ class Player
   def bet_request(game_state)
     minimum_raise = 0
 
-    game_state[:current_buy_in] - game_state[:players][:in_action][:bet]  + minimum_raise
+    game_state['current_buy_in'] - game_state['players']['in_action']['bet']  + minimum_raise
   end
 
   def showdown(game_state)
