257c94f91bc7f4613d3f7254529de9d794432a96
butumusho <danuljak@gmail.com>
2016-03-26 10:51:32 +0200

    3

--- player.rb
+++ player.rb
@@ -4,7 +4,8 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    game_state['current_buy_in']
+    minimum_raise = 0
+    game_state['current_buy_in'] - game_state['players']['in_action']['bet']  + minimum_raise
   end
 
   def showdown(game_state)
